d0d4b03115d3e78f2d76d950f37087f585dd833c
Keshav-Maheshwari <maheshwarikeshav029@gmail.com>
2025-04-22 20:11:34 +0530

    Integrated Payment gateway

---
+++ src/app/test/payment/page.js
@@ -0,0 +1,12 @@
+"use client"
+import PaymentForm from "@/components/PaymentForm";
+import { useState } from "react";
+
+export default function ParentComponent() {
+  const [formData, setFormData] = useState({ name: "Keshav", email: "keshav029@gmail.com", phone: "9993312857" });
+  const [loading, setLoading] = useState(false);
+  const amount = 100;
+  const getId = () => Date.now().toString();
+
+  return <PaymentForm formData={formData} amount={amount} getId={getId} setLoading={setLoading} />;
+}
