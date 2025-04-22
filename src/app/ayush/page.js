3074d5ef30a00fe5ddd71fa689e0d0d6dcb4d910
Keshav-Maheshwari <maheshwarikeshav029@gmail.com>
2025-04-22 09:10:09 +0530

    added routes for Scheme

--- src/app/ayush/page.js
+++ src/app/ayush/page.js
@@ -1,4 +1,5 @@
-import SchemeExplorer from '@/components/SchemaExplorer'
+import SchemeExplorer from "./scheme/page"
+
 
 
 const page = async () => {
--- src/components/schemes/SchemeCard.jsx
+++ src/components/schemes/SchemeCard.jsx
@@ -1,10 +1,16 @@
 "use client";
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
+import { useRouter } from "next/navigation";
 
 export default function SchemeCard({ scheme }) {
+  const router = useRouter();
+  const handleClick = () => {
+    
+    router.push(`/ayush/scheme/${scheme.id}`);
+  };
   return (
-    <Card key={scheme.title} className="shadow-md cursor-pointer">
+    <Card key={scheme.title} className="shadow-md cursor-pointer"onClick={handleClick}>
       <CardContent className="p-4 space-y-2">
         <h3 className="text-lg font-semibold">{scheme.title}</h3>
         <p className="text-sm text-muted-foreground">{scheme.location}</p>
